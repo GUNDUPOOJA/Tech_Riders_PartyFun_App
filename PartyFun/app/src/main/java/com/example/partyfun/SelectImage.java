7bebae43b1553b3e9e6023c2bd513c686b5270ce
sharadruthi <druthi.beerukuri@gmail.com>
2019-10-04 22:24:57 -0500

    Sharadruthi ----Intent created for play area and select image

--- PartyFun/app/src/main/java/com/example/partyfun/SelectImage.java
+++ PartyFun/app/src/main/java/com/example/partyfun/SelectImage.java
@@ -2,7 +2,9 @@ package com.example.partyfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectImage extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class SelectImage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_image);
     }
+
+    public void onImage (View v){
+        Intent image = new Intent(this,PlayArea.class);
+        startActivity(image);
+    }
 }
+
