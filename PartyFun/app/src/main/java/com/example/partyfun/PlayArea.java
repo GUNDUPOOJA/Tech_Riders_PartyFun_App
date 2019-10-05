9749136fef8afedb319aacf4c0b73b468289abd9
sharadruthi <druthi.beerukuri@gmail.com>
2019-10-04 22:36:45 -0500

    Sharadruthi  ----Intent created for play area.

--- PartyFun/app/src/main/java/com/example/partyfun/PlayArea.java
+++ PartyFun/app/src/main/java/com/example/partyfun/PlayArea.java
@@ -2,7 +2,9 @@ package com.example.partyfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayArea extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PlayArea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_area);
+    }
+
+    public void onGo(View V){
+        Intent onGo = new Intent(this,TruthOrDare.class);
+        startActivity(onGo);
     }
 }
