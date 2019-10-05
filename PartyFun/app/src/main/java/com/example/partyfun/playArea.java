284d219c977909348efb8b4378fe4adbfec0eca4
sowmya <sowmyakumbham97@gmail.com>
2019-10-04 22:01:12 -0500

    Sowmya: Play Area layout

---
+++ PartyFun/app/src/main/java/com/example/partyfun/playArea.java
@@ -0,0 +1,14 @@
+package com.example.partyfun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_area);
+    }
+}
