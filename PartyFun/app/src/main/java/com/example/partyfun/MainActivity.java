453d6eaef68db40f6eefae12ac1c4e8b2e343dbf
SOWMYAREDDY97 <46725519+SOWMYAREDDY97@users.noreply.github.com>
2019-10-24 22:55:53 -0500

    Sowmya : Start of the app with animation

--- PartyFun/app/src/main/java/com/example/partyfun/MainActivity.java
+++ PartyFun/app/src/main/java/com/example/partyfun/MainActivity.java
@@ -4,24 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int timeout = 3000;
+    TextView txt ;
+    ImageView img;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.txt);
+        img=findViewById(R.id.img);
+
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animi);
+        img.startAnimation(animation);
+        txt.startAnimation(animation);
+
+        Handler handler = new Handler ();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity1.class);
+                startActivity(intent);
+
+            }
+        },timeout);
+
     }
-
-    public void onClick(View v) {
-        Intent intent_special = new Intent(this, SpecialOne.class);
-        startActivity(intent_special);
-
-    }
-
-
 }
-
-
---
+++ PartyFun/app/src/main/java/com/example/partyfun/MainActivity1.java
@@ -0,0 +1,27 @@
+package com.example.partyfun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+
+    public void onClick(View v) {
+        Intent intent_special = new Intent(this, SpecialOne.class);
+        startActivity(intent_special);
+
+    }
+
+
+}
+
+
