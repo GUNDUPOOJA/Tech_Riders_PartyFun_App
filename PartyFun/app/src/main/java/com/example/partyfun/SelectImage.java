36fa71f44a39c804ef78dbc064fcd017f7e61abb
sharadruthi <druthi.beerukuri@gmail.com>
2019-10-25 23:52:24 -0500

    Sharadruthi--- Browse button. Picks image from gallery of the phone

--- PartyFun/app/src/main/java/com/example/partyfun/SelectImage.java
+++ PartyFun/app/src/main/java/com/example/partyfun/SelectImage.java
@@ -3,11 +3,17 @@ package com.example.partyfun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SelectImage extends AppCompatActivity {
-
+    ImageView imageView;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,30 @@ public class SelectImage extends AppCompatActivity {
     public void onImage (View v){
         Intent image = new Intent(this,PlayArea.class);
         startActivity(image);
+    }
+
+    public void onBrowse(View v){
+        imageView = findViewById(R.id.imageView2);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openGallery();
+            }
+        });
+    }
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+
     }
 }
 
