c09855efead8a0951e10e318e498e7b7de16a1ae
DeepakJayanthi <46684265+DeepakJayanthi@users.noreply.github.com>
2019-10-29 22:13:09 -0500

    Deepak: functionality to enter player name is done

--- PartyFun/app/src/main/java/com/example/partyfun/MainActivity1.java
+++ PartyFun/app/src/main/java/com/example/partyfun/MainActivity1.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity1 extends AppCompatActivity {
     private Model player_model;
@@ -32,29 +33,54 @@ public class MainActivity1 extends AppCompatActivity {
         playerrecycler.setAdapter(playerServer);
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         playerrecycler.setLayoutManager(linearLayoutManager);
+
+        Button word_add = findViewById(R.id.button);
+        EditText add_player = findViewById(R.id.addBTN);
+
+        /*String str = add_player.getText().toString();
+        word_add.setEnabled(true);
+        if(str.equals(" ")|| str == null);
+        {
+            word_add.setEnabled(false);
+        }*/
+
         playerrecycler.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener(){
             @Override
             public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
                 return gesture_detector.onTouchEvent(e);
             }
         });
-        Button word_add = findViewById(R.id.button);
+      //  Button word_add = findViewById(R.id.button);
 
         word_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText add_player = findViewById(R.id.addBTN);
-
-
                 String str = add_player.getText().toString();
-                player_model.getPlayersarray().add(new Model.Player(str));
 
-                playerServer.notifyItemInserted(player_model.getPlayersarray().size() - 1);
-                add_player.getText().clear();
+
+
+                if (str.length()==0){
+                    Toast.makeText(MainActivity1.this, "Enter the player name " + str, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity1.this, "Name is succesfully  is entered", Toast.LENGTH_SHORT).show();
+                    player_model.getPlayersarray().add(new Model.Player(str));
+
+                    playerServer.notifyItemInserted(player_model.getPlayersarray().size() - 1);
+                    add_player.getText().clear();
+                }
+
+
+
             }
+
 
         });
     }
+
+
+
 
     public void onClick(View v) {
         Intent intent_special = new Intent(this, SpecialOne.class);
