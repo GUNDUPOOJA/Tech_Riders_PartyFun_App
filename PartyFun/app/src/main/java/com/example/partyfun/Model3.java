019d342ce7600b4e7d07d172e0a52d52512a29fb
Chaitra543 <chaitravemula2019@gmail.com>
2019-11-15 15:34:58 -0600

    added recyclerview to score>>chaitra

---
+++ PartyFun/app/src/main/java/com/example/partyfun/Model3.java
@@ -0,0 +1,44 @@
+package com.example.partyfun;
+
+import java.util.ArrayList;
+
+public class Model3 {
+    public ArrayList<Score> getScoresarray() {
+        return Scoresarray;
+    }
+
+    //setter for arraylist
+    public void setScoresarray(ArrayList<Score> Scoresarray) {
+
+        this.Scoresarray = Scoresarray;
+    }
+
+    //creating a class for words in recyclerview
+    public static class Score {
+        public String Score;
+
+        public Score(String Score) {
+            this.Score = Score;
+
+        }
+    }
+
+
+    private static Model3 my_Model = null;
+
+    public static Model3 getModel() {
+        if (my_Model == null) {
+            my_Model = new Model3();
+        }
+        return my_Model;
+    }
+
+    private ArrayList<Score> Scoresarray;
+
+    private Model3() {
+        setScoresarray(new ArrayList<Score>());
+//        loadModel();
+    }
+
+
+}
