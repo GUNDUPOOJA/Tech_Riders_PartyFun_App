ce27c976c0c7ca5203bb0b746452d80366c3f7d3
sharadruthi <druthi.beerukuri@gmail.com>
2019-11-15 19:43:51 -0600

    Sharadruthi ---New activity created for truth and added intents for dare

--- PartyFun/app/src/main/java/com/example/partyfun/PlayArea.java
+++ PartyFun/app/src/main/java/com/example/partyfun/PlayArea.java
@@ -2,6 +2,7 @@ package com.example.partyfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,99 +26,55 @@ public class PlayArea extends AppCompatActivity {
         setContentView(R.layout.activity_play_area);
         Button truth=findViewById(R.id.truth);
         Button dare = findViewById(R.id.dare);
-//        truth.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                PlayDialog mad = new PlayDialog();
 
-                //
-
-//                final TextView a = findViewById(R.id.tv);
-//                ParseQuery<ParseObject> query = ParseQuery.getQuery("Truth");
-//                query.findInBackground(new FindCallback<ParseObject>(){
-//                                           public void done(List<ParseObject> truthList, ParseException e) {
-//                                               if (e == null) {
-//                                                   for (int i = 0; i < truthList.size(); i++) {
-//                                                       System.out.println(truthList.get(i).get("Truth"));
-//                                                       a.setText(""+truthList.get(i).get("Truth"));
-//                                                   }
-//                                               } else {
-//                                                   Log.d("Parse", "Error: " + e.getMessage());
-//                                               }
-//                                           }
-//                                       }
-//                );
-                //SharedPreferences pref = getApplicationContext().getSharedPreferences("emotionValue", 0);
-                // SharedPreferences.Editor editor = pref.edit();
-                //editor.putInt("key_name", -1);
-//                mad.show(getSupportFragmentManager(),"Emotion Dialog Box");
-
-//            }
-//        });
-
-//        dare.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                PlayDialog mad = new PlayDialog();
-//                //SharedPreferences pref = getApplicationContext().getSharedPreferences("emotionValue", 0);
-//                // SharedPreferences.Editor editor = pref.edit();
-//                //editor.putInt("key_name", -1);
-//                mad.show(getSupportFragmentManager(),"Emotion Dialog Box");
-//
-//            }
-//        });
-//
-//
-//    }
-
-
-
-//    @Override
-//    public void done() {
-//
-//    };
-//
-//    @Override
-//    public void forfeit() {
-//
    };
 
 
     public void onTruth(View v) {
-        //Button getBTN = findViewById(R.id.test);
-        final ArrayList<Object> Tlist  = new ArrayList<>();
-
-        final TextView a = findViewById(R.id.tv);
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Truth");
-        final Random r = new Random();
-
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> truthList, ParseException e) {
-                                       if (e == null) {
-                                           for (int i = 0; i < truthList.size(); i++) {
-                                            System.out.println(truthList.get(i).get("Truth"));
-
-                                               Tlist.add(" "+truthList.get(i).get("Truth"));
-                                           //    a.setText("truth" + truthList.get(i).get("Truth"));
-                                               System.out.println("abc");
-                                               System.out.println(Tlist.size());
-                                           }
-                                           System.out.println("dffafafdg");
-                                           System.out.println(Tlist);
-                                           Object abc = Tlist.get(r.nextInt(Tlist.size()));
-                                           a.setText(""+ abc);
-                                       } else {
-                                           Log.d("Parse", "Error: " + e.getMessage());
-                                       }
-                                   }
-                               }
-
-        );
-
-
-
+        Intent t = new Intent(this,tru.class);
+        startActivity(t);
     }
 
+    public void onDare(View v) {
+        Intent D = new Intent(this,tru.class);
+        startActivity(D);
+    }
+};
+        //Button getBTN = findViewById(R.id.test);
+//        final ArrayList<Object> Tlist  = new ArrayList<>();
+//
+//        final TextView a = findViewById(R.id.tv);
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Truth");
+//        final Random r = new Random();
+//
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            public void done(List<ParseObject> truthList, ParseException e) {
+//                                       if (e == null) {
+//                                           for (int i = 0; i < truthList.size(); i++) {
+//                                            System.out.println(truthList.get(i).get("Truth"));
+//
+//                                               Tlist.add(" "+truthList.get(i).get("Truth"));
+//                                           //    a.setText("truth" + truthList.get(i).get("Truth"));
+//                                               System.out.println("abc");
+//                                               System.out.println(Tlist.size());
+//                                           }
+//                                           System.out.println("dffafafdg");
+//                                           System.out.println(Tlist);
+//                                           Object abc = Tlist.get(r.nextInt(Tlist.size()));
+//                                           a.setText(""+ abc);
+//                                       } else {
+//                                           Log.d("Parse", "Error: " + e.getMessage());
+//                                       }
+//                                   }
+//                               }
+//
+//        );
+//
+//
+//
+//    }
+//
+//
 
 
 
@@ -125,7 +82,6 @@ public class PlayArea extends AppCompatActivity {
 
 
 
-    };
 
 
 
--- PartyFun/app/src/main/java/com/example/partyfun/tru.java
+++ PartyFun/app/src/main/java/com/example/partyfun/tru.java
@@ -2,7 +2,20 @@ package com.example.partyfun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class tru extends AppCompatActivity {
 
@@ -10,5 +23,46 @@ public class tru extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tru);
+        final ArrayList<Object> Tlist  = new ArrayList<>();
+
+        final TextView a = findViewById(R.id.textView2);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Truth");
+        final Random r = new Random();
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+                                   public void done(List<ParseObject> truthList, ParseException e) {
+                                       if (e == null) {
+                                           for (int i = 0; i < truthList.size(); i++) {
+                                               System.out.println(truthList.get(i).get("Truth"));
+
+                                               Tlist.add(" "+truthList.get(i).get("Truth"));
+                                               //    a.setText("truth" + truthList.get(i).get("Truth"));
+                                               System.out.println("abc");
+                                               System.out.println(Tlist.size());
+                                           }
+                                           System.out.println("dffafafdg");
+                                           System.out.println(Tlist);
+                                           Object abc = Tlist.get(r.nextInt(Tlist.size()));
+                                           a.setText(""+ abc);
+                                       } else {
+                                           Log.d("Parse", "Error: " + e.getMessage());
+                                       }
+                                   }
+                               }
+
+        );
+
+
+
+    }
+
+    public void onDone(View v) {
+        Intent D = new Intent(this,PlayArea.class);
+        startActivity(D);
+    }
+    public void onForfeit(View v) {
+        Intent F = new Intent(this,PlayArea.class);
+        startActivity(F);
     }
 }
+
