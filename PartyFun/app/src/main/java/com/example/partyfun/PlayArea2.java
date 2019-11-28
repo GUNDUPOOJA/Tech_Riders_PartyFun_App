9f6d7523f54902214f145ae32871599bf5247f76
SOWMYAREDDY97 <46725519+SOWMYAREDDY97@users.noreply.github.com>
2019-11-27 22:29:47 -0600

    sowmya : play area game is done and random image is picked on clicking a button.

--- PartyFun/app/src/main/java/com/example/partyfun/PlayArea2.java
+++ PartyFun/app/src/main/java/com/example/partyfun/PlayArea2.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class PlayArea2 extends AppCompatActivity {
-
+    final Handler handler = new Handler();
+    Runnable runnable;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +22,31 @@ public class PlayArea2 extends AppCompatActivity {
         Button truth=findViewById(R.id.truth);
         Button dare = findViewById(R.id.dare);
 
-    };
+        imageView = (ImageView) findViewById(R.id.imageView);
+        final int[] imageArray =
+                {R.drawable.men1, R.drawable.men2, R.drawable.men3,R.drawable.woman1, R.drawable.woman2, R.drawable.women3};
 
+
+        Random r = new Random();
+        int timer = r.nextInt(200 - 100) + 30;
+
+
+        runnable = new Runnable() {
+            int i = 0;
+
+            public void run() {
+                imageView.setImageResource(imageArray[i]);
+                i++;
+                if (i > imageArray.length - 1) {
+                    i = 0;
+                }
+                handler.postDelayed(this, 100);  //for interval...
+            }
+
+        };
+        handler.postDelayed(runnable, 100); //for initial delay..
+
+    };
 
     public void onTruth(View v) {
         Intent t = new Intent(this, Tru1.class);
@@ -28,7 +57,13 @@ public class PlayArea2 extends AppCompatActivity {
         Intent D = new Intent(this, Dar1.class);
         startActivity(D);
     }
-};
+
+    public void onclick(View v) {
+
+        handler.removeCallbacks(runnable);
+
+    }
+}
 //Button getBTN = findViewById(R.id.test);
 //        final ArrayList<Object> Tlist  = new ArrayList<>();
 //
